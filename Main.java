ec0f347acad809a8d8e4951c19323461df36cb27
Christopher J Wilson <cjwilson2020@gmail.com>
2021-01-02 23:35:00 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,51 @@
+import java.util.HashMap;
+
+class Main {
+  public static void main(String[] args) {
+    //Google Question
+    // Given an array = [2,5,2,3,3,5,1,2,4]
+    // It should return 2
+
+    //Given an array = [2,1,1,2,3,5,1,2,4]
+    //It should return 1
+
+    //Given an array = [2,3,4,5]
+    // It shoule return null
+
+    Integer[] a1 = {2,5,2,3,3,5,1,2,4};
+    Integer[] a2 = {2,1,1,2,3,5,1,2,4};
+    Integer[] a3 = {2,3,4,5};
+
+    Integer first = getFirstRepeat(a1);
+    if (first == null){
+      System.out.println("No repeating numbers found\ngetFirstRepeat returned " + first);
+    }else{
+      System.out.println("The first number to repeat in the given array is " + first);
+    }
+  }
+
+  static Integer getFirstRepeat(Integer[] a){
+    HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+    int cnt = 0;
+    for (int i = 0; i < a.length; i++){
+      if (!a[i].equals(null)){
+        cnt++;
+        if (map.containsKey(a[i])){
+          // System.out.println(a[i]+" found in HashMap!");
+          // System.out.println("Value: " + map.get(a[i]));
+          map.put(a[i], map.get(a[i]) + 1);
+          cnt = 0;
+         // System.out.println("New value: " + map.get(a[i]));
+          return a[i];
+        } else{
+          map.put(a[i], cnt);
+          cnt = 0;
+        }
+      }
+    }
+    // System.out.println("Keys in map" + map.keySet().toString());
+    // System.out.println("Values in map" + map.values().toString());
+
+    return null;
+  }
+}
